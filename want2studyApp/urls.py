dcfe4a35350a50b7cc8e1672b091eb745f06a847
yamayuz <yuki@yamayulab.com>
2022-02-16 00:10:30 +0900

    【update】サインイン、サインアップ、ログアウト機能の追加

--- want2studyApp/urls.py
+++ want2studyApp/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
-from .views import StudylistView, DetailView, ChangeFinishView, DeleteView, UpdateView
+from .views import StudylistView, DetailView, ChangeFinishView, DeleteView, UpdateView, SignupView, SigninView, LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', StudylistView.as_view(), name='studylist'),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('signin/', SigninView.as_view(), name='signin'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('list/', StudylistView.as_view(), name='studylist'),
     path('detail/<int:pk>', DetailView.as_view(), name='detail'),
     path('changeFinish/<int:pk>', ChangeFinishView.as_view(), name='changefinish'),
     path('delete/<int:pk>', DeleteView.as_view(), name='delete'),
     path('update/<int:pk>', UpdateView.as_view(), name='update'),
+    
 ] 
--- want2studyApp/views.py
+++ want2studyApp/views.py
@@ -1,21 +1,66 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, View, DeleteView, UpdateView
+from django.views.generic import ListView, DetailView, View, DeleteView, UpdateView, View
 from .models import studyModel
 import datetime
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
-class StudylistView(ListView):
-    template_name = 'studylist.html'
-    model = studyModel
+class SignupView(View):
+    def get(self, request):
+        return render(request, 'signup.html')
 
     def post(self, request):
+        target_username = request.POST['username']
+        target_password = request.POST['password']
+        try:
+            User.objects.get(username=target_username)
+            return render(request, 'signup.html', {'error': 'このユーザーは登録されています'})
+        except:
+            user = User.objects.create_user(target_username, '', target_password)
+            return redirect('studylist')
+
+
+class SigninView(View):
+    def get(self, request):
+        return render(request, 'signin.html')
+
+    def post(self, request):
+        target_username = request.POST['username']
+        target_password = request.POST['password']
+        user = authenticate(username=target_username, password=target_password)
+        if user is not None:
+            login(request, user)
+            return redirect('studylist')
+        else:
+            return render(request, 'signin.html', {'error': '登録されていないユーザーです。\nユーザーを新規登録してください'})
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return render(request, 'signin.html', {'message': 'ログアウトしました。'})
+
+
+@method_decorator(login_required, name='dispatch')
+class StudylistView(ListView):
+    template_name = 'studylist.html'
+
+    def get_queryset(self):
+        current_user = self.request.user
+        return studyModel.objects.filter(user=current_user.username)
+
+    def post(self, request):
+        current_user = self.request.user
         models = studyModel(
                         title = request.POST["title"], 
                         detail = "",
                         finishFlg = False,
                         created = datetime.datetime.now(),
-                        user = "",
+                        user = current_user.username,
                         category = request.POST["category"]
                         )
         models.save()
